d428657b38fc2abd37675ad8f5a6de734a1ec472
mattkearl <mkearl@gmail.com>
2025-01-10 21:38:22 -0700

    copy 131 class

---
+++ starts.js
@@ -0,0 +1,8 @@
+//const blockStart = new Date('1/22/2023').getTime();
+export const blockStart = new Date('01/06/2025').getTime(); // random day
+export const week2start = new Date('01/13/2025').getTime(); // first Monday after block starts
+export const blockEnd = new Date('02/22/2025').getTime();
+
+export const monthList= ["January","February","March","April","May","June","July",
+"August","September","October","November","December"];
+
